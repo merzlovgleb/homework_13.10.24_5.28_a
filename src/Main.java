1f52ecafb6dbe7deed9c2d29b62584693407a68a
merzlovgleb <merzlovgleb@gmail.com>
2024-10-19 14:40:05 +0300

    5.28_a

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int mul = 1;
+        for (int c =8; c <= 15; c++) {
+            mul = mul * c;
+        };
+        System.out.println(mul);
+    }
+}
